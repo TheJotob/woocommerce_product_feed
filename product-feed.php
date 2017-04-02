dae81dacdaadf1770762bdc30854b9e9f365c866
Joachim Eckerlin <joachim.eckerlin@gmail.com>
2017-04-02 13:19:00 +0200

    Updated description

--- product-feed.php
+++ product-feed.php
@@ -1,11 +1,20 @@
 <?php
 
 /*
-Plugin Name: Product Feed
+Plugin Name: Product Feeds
 Plugin URI: http://URI_Of_Page_Describing_Plugin_and_Updates
 Description: A brief description of the Plugin.
 Version: 1.0
-Author: joach
-Author URI: http://URI_Of_The_Plugin_Author
+Author: Laitenberger Eckerlin
+Author URI: http://laitenberger-eckerlin.de
 License: A "Slug" license name e.g. GPL2
 */
+
+// Register Feed Post Type
+add_action('init', function() {
+    $args = [
+        'public'    => true,
+        'label'     => "Feeds"
+    ];
+    register_post_type('feed', $args);
+});
